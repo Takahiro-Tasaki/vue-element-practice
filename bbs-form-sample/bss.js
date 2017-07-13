b19ed1648cea4e7cc6f3e4a8ec2cddbc6ecd3612
Takahiro-Tasaki <tsktkhr0316@icloud.com>
2017-07-13 16:30:15 +0900

    BBSのサンプル作成

---
+++ bbs-form-sample/bss.js
@@ -0,0 +1,76 @@
+'use strict';
+
+var storageName = 'dataBBS';
+var arrayList   = [];
+
+var Main    = {
+  data: function () {
+    return {
+      labelPosition: 'right',
+      labelWidth: '100px',
+      formData: {
+        name: '',
+        delivery: false,
+        email: '',
+        title: '',
+        message: '',
+        password: ''
+      },
+      rules: {
+        name: [
+          {required: true, message: 'name is required'}
+        ],
+        title: [
+          {required: true, message: 'title is required'}
+        ],
+        message: [
+          {required: true, message: 'message is required'}
+        ],
+        password: [
+          {required: true, password: 'title is required'}
+        ]
+      }
+    };
+  },
+  methods: {
+    submitForm: function (formName) {
+      var scope = this;
+      this.$refs[formName].validate(function (valid) {
+        if (valid) {
+          scope.pushForm();
+        } else {
+          alert('error submit!!');
+          return false;
+        }
+      });
+    },
+    resetForm: function (formName) {
+      this.$refs[formName].resetFields();
+    },
+    pushForm: function () {
+      arrayList.push({
+        'name'    : this.formData.name,
+        'delivery': this.formData.delivery,
+        'email'   : this.formData.email,
+        'title'   : this.formData.title,
+        'message' : this.formData.message,
+        'password': this.formData.password
+      });
+      localStorage.setItem(storageName, JSON.stringify(arrayList));
+      console.log(arrayList);
+    }
+  }
+};
+var vueForm = Vue.extend(Main);
+new vueForm().$mount('#app');
+
+arrayList = localStorage.getItem(storageName);
+arrayList = JSON.parse(arrayList);
+if(!arrayList)arrayList = [];
+
+var messageList = new Vue({
+  el : '#message-box',
+  data: {
+    arrayItems: arrayList
+  }
+});
